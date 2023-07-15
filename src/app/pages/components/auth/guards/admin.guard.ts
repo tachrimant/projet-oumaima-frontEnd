af73550977283f0d977260bf9b8e73bec3b5a69d
ahmeddahbi <dahbiahmed1999@gmail.com>
2023-07-15 03:51:31 +0100

    dz

---
+++ src/app/pages/components/auth/guards/admin.guard.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import {AuthService} from "../auth.service";
+import {AppStore} from "../../../services/app-store.service";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class AdminGuard implements CanActivate {
+    constructor(private authService: AuthService,private appStore:AppStore, private router: Router) {}
+
+    canActivate(): boolean {
+
+        if (this.authService.isLogged() ) {
+            let element;
+        this.appStore.getUser().subscribe((res)=>{
+            if(res.authorities[0]==='ROLE_ADMIN')
+                element= true
+            else element= false
+
+        })
+            return element
+         } else {
+            this.router.navigate(['/login']);
+            return false;
+        }
+    }
+}
